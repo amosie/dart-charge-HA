3b2cf7a1ea10eeaca056ae6a6ed13cf50ca2e9bb
Andy Dennis <andy@dennisimo.com>
2014-10-10 08:14:52 +0100

    html drop for account setup. also has js for dart tag verification

--- app/routes.js
+++ app/routes.js
@@ -1,22 +1,27 @@
 module.exports = {
   bind : function (app, assetPath) {
     app.get('/', function (req, res) {
-      res.render('index',
-                {'assetPath' : assetPath});
+      res.render('index', {'assetPath' : assetPath});
     });
 
-    /* Example pages */
-
-    app.get('/examples/hello-world', function (req, res) {
-      res.render('examples/hello-world', {'message' : 'Hello world'});
+    app.post('/account/setup/success', function (req, res) {
+      res.render('account/setup/success', {'assetPath' : assetPath});
     });
 
-    app.get('/examples/inheritance', function (req, res) {
-      res.render('examples/inheritance/page-level', {'message' : 'Hello world'});
+    app.post('/account/setup/personal', function (req, res) {
+      res.render('account/setup/personal/index', {'assetPath' : assetPath});
     });
 
-    app.get('/examples/alpha', function (req, res) {
-      res.render('examples/alpha/alpha', {'assetPath' : assetPath });    
+    app.post('/account/setup/vehicle', function (req, res) {
+      res.render('account/setup/vehicle/index', {'assetPath' : assetPath});
+    });
+
+    app.post('/account/setup/payment', function (req, res) {
+      res.render('account/setup/payment/index', {'assetPath' : assetPath});
+    });
+
+    app.post('/account/setup/payment/card', function (req, res) {
+      res.render('account/setup/payment/card', {'assetPath' : assetPath});
     });
 
   }
