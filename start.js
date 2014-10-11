318fc8c149c18050f2fd7c88b1a2b85a80d1004f
Andy Dennis <andy@dennisimo.com>
2014-10-11 12:01:47 +0100

    modified start.js to run with ruby by default. hopefully this will resolve heroku display issues

--- start.js
+++ start.js
@@ -5,10 +5,16 @@ var fs = require('fs'),
     gruntfile;
 
 // start grunt
-gruntfile = (argv.ruby) ? __dirname + '/Gruntfile_ruby_sass.js' : __dirname + '/Gruntfile.js';
+gruntfile = __dirname + '/Gruntfile_ruby_sass.js';
 require(__dirname + '/node_modules/grunt/lib/grunt.js').cli({
   'gruntfile' : gruntfile
 });
+
+// // start grunt
+// gruntfile = (argv.ruby) ? __dirname + '/Gruntfile_ruby_sass.js' : __dirname + '/Gruntfile.js';
+// require(__dirname + '/node_modules/grunt/lib/grunt.js').cli({
+//   'gruntfile' : gruntfile
+// });
 
 fs.writeFileSync(pidFile, process.pid, fileOptions);
 
